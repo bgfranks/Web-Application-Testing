079f9d1cf4a067a5401ab9f762e061d16492e9bf
Brandon Franks <bgfranks106@icloud.com>
2020-01-23 21:32:52 -0500

    wrote 4 basic unit tests

--- baseball-testing/src/Components/Dashboard.test.js
+++ baseball-testing/src/Components/Dashboard.test.js
@@ -1,4 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { addCount, foulCount } from "./Dashboard";
 
-import Dashboard, { addCount, foulCount, clearCount } from "./Dashboard";
+test("adds to the count by 1", () => {
+  const currentCount = 1;
+  const expected = 2;
+
+  const actual = addCount(currentCount);
+
+  expect(actual).toBe(expected);
+});
+
+test("adds to count by one unless count is 2", () => {
+  const currentCount = 2;
+  const expected = 2;
+
+  const actual = foulCount(currentCount);
+
+  expect(actual).toBe(expected);
+});
